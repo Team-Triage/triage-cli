32735f77bfd162ca76760b959cfa8afafc4d7243
mhtjung <themikejung@gmail.com>
2022-11-16 15:47:37 -0800

    Add fresh download of triage on init
    Fresh download of triage repo on triage init
    
    Co-Authored-By: Aashish Balaji <87250229+aashish-b@users.noreply.github.com>
    Co-Authored-By: Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
    Co-Authored-By: Jordan Swartz <10202436+jordanLswartz@users.noreply.github.com>

--- cli/src/init.js
+++ cli/src/init.js
@@ -1,8 +1,11 @@
 import createConfigFile from '../utils/createConfigFile'
-
+import executeCommand from '../utils/executeCommand'
 function init () {
+  console.log("Creating config file: config.properties!")
   createConfigFile('config.properties')
-  console.log("config.properties created!")
+  
+  console.log("Getting the latest version of Triage")
+  executeCommand("git clone https://github.com/team-triage/triage.git src && cd src && rm -rf .git")
 }
 
 export default init
