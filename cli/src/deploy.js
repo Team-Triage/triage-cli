9dc99e3afd91cd4e4c204a33b59e930bd1b7b9e7
mhtjung <themikejung@gmail.com>
2022-11-16 20:00:43 -0800

    Update deploy.js
    Co-Authored-By: Aashish Balaji <87250229+aashish-b@users.noreply.github.com>
    Co-Authored-By: Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
    Co-Authored-By: Jordan Swartz <10202436+jordanLswartz@users.noreply.github.com>

--- cli/src/deploy.js
+++ cli/src/deploy.js
@@ -25,12 +25,3 @@ function deploy() {
 }
 
 export default deploy
-
-/*
-1. copy config properties into triage src
-2. add auth token to config.properties
-3. create new image with docker build
-4. cdk prep 0- we already did this??
-5. take topic name and interpolate it into cdk stack js file inside cd/lib
-
-*/
